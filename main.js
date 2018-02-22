e0ad523d416d7c4de729ef400ad8884812841d69
银翘 <yinqiao.zrj@alibaba-inc.com>
2018-02-22 19:43:50 +0800

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+var count1 = require('./counter');
+var count2 = require('./counter');
+
+console.log(count1.count());
+console.log(count2.count());
+console.log(count2.count());
+console.log(count2.count());
